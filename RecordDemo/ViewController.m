d00803fa0eac685d57ecf40c88c132a803d69b61
xieyuecong <xieyuecong@0easy.com>
2018-12-10 14:32:04 +0800

    Initial Commit

---
+++ RecordDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  RecordDemo
+//
+//  Created by SZOeasy on 2018/12/10.
+//  Copyright © 2018年 yilingyijia. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
